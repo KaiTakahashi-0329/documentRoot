9254e6ab210369c89275d4bb767f77cda80cad94
KaiTakahashi-0329 <kai.takahashi329@gmail.com>
2022-03-27 23:03:28 +0900

    Add migrations file

---
+++ ToDoApp/database/migrations/2022_03_27_125639_create_todos_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('title', 50);
+            $table->text('text')->nullable();;
+            $table->string('important', 10);
+            $table->string('urgent', 10);
+            $table->foreignId('status_id')->constrained('statuses');
+            $table->dateTime('deadline')->nullable();
+
+            /**
+             * user_id
+             * title
+             * text
+             * Important 重要度
+             * urgent 緊急度
+             * status ステータス
+             * deadline 期限
+             */
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
