51e18054ac15a96357d63beca6be78e3e0b242e7
KaiTakahashi-0329 <kai.takahashi329@gmail.com>
2022-03-28 19:54:30 +0900

    todo 一覧のapiを叩くように変更

--- ToDoApp/resources/js/components/List.jsx
+++ ToDoApp/resources/js/components/List.jsx
@@ -2,7 +2,7 @@ const List = (props) => {
     const { children } = props
 
     return (
-        <ul className="row row-cols-1 row-cols-md-3 g-4 list-unstyled">
+        <ul className="row row-cols-1 row-cols-md-3 mt-3 list-unstyled">
             { children }
         </ul>
     )
--- ToDoApp/resources/js/templates/Top.jsx
+++ ToDoApp/resources/js/templates/Top.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 
 import List from '../components/List';
 import Item from '../components/Item';
 import PrimaryButton from '../components/PrimaryButton';
+import axios from 'axios';
 
 function Top() {
+    const [todoList, setTodoList] = useState([]);
+
+    useEffect(() => {
+        const fetchTodoList = async () => {
+            await axios.get('/api/todos').then(res => {
+                setTodoList(res.data);    
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        } 
+
+        fetchTodoList();
+    }, []);
+
     return (
         <>
             <Link to="/create">
                 <PrimaryButton text="追加する" />
             </Link>
             <List>
-                <Item />
-                <Item />
+                {
+                    todoList.map((element, index) => {
+                        return <Item key={index} id={element.id} title={element.title} text={element.text} created_at={element.created_at} />
+                    })
+                }
             </List>
         </>      
     );
