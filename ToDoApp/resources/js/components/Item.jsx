bf0f9b5f6082cb2bf9e892e415166a8871b0aa19
KaiTakahashi-0329 <kai.takahashi329@gmail.com>
2022-04-05 23:24:15 +0900

    Add edit link

--- ToDoApp/resources/js/components/Item.jsx
+++ ToDoApp/resources/js/components/Item.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 const item = (props) => {
     const { id, title, text, created_at } = props;
     const link = `/todo/${id}`;
+    const editLink = `/todo/edit/${id}`;
 
     return (
         <li className="col">
@@ -10,8 +11,8 @@ const item = (props) => {
                 <div className="card-body">
                     <h4 className="card-title">{ title }<span class="badge bg-secondary ms-2">New</span></h4>
                     <p className="card-text">{ text }</p>
-                    <Link to={link} className="card-text">More View</Link>
-                    
+                    <Link to={link} className="card-text">詳細を見る</Link>
+                    <Link to={editLink} className="card-text ms-3">編集する</Link>
                 </div>
                 <div className="card-footer">
                     <small className="text-muted">{created_at}</small>
