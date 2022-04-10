a072cd69bbb02174d0d48c1a7cb8fccd3087a441
KaiTakahashi-0329 <kai.takahashi329@gmail.com>
2022-04-10 14:30:41 +0900

    Add Edit.jsx

--- ToDoApp/resources/js/templates/Edit.jsx
+++ ToDoApp/resources/js/templates/Edit.jsx
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import Form from '../components/Form';
 import PostResultMessage from '../components/PostResultMessage';
 
 import useFetchTodo from '../hooks/useFetchTodo';
+import axios from 'axios';
 
 const Edit = (props) => {
     const {isPostResult, postResult} = props;
-
+    const navigate = useNavigate();
     const params = useParams();
     const id = params.id;
+    
+    const submitForm = (data) => {
+        axios.post(`/api/todo/update/${id}`, data)
+        .then(() => {
+            isPostResult('success')
+            navigate('/')
+        })
+        .catch((error) => {
+            console.log(error);
+            isPostResult('error')
+        })
+    }
+
     const todo = useFetchTodo(id);
 
     useEffect(() => {
@@ -21,7 +35,7 @@ const Edit = (props) => {
         <>
             <PostResultMessage postResult={postResult} />
             <div className="container w-75">
-                <Form todo={todo} />
+                <Form todo={todo} onClickAddButton={submitForm} />
             </div>
         </>
     )
