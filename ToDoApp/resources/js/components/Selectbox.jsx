a4b07d68ae885c0be590f528e45e08f209adb426
KaiTakahashi-0329 <kai.takahashi329@gmail.com>
2022-04-11 23:00:49 +0900

    Select Component改修

--- ToDoApp/resources/js/components/Selectbox.jsx
+++ ToDoApp/resources/js/components/Selectbox.jsx
@@ -1,14 +1,14 @@
 const Selectbox = (props) => {
-    const { label, selectArray, onChange, value = 1 } = props;
+    const { label, selectArray, onChange, value } = props;
 
     const options = selectArray.map((element, index) => {
         return <option key={index} value={index + 1}>{ element }</option>
-    });
+    });    
 
     return (
         <>
             <label className="form-label">{ label }</label>
-            <select class="form-select" defaultValue={value} onChange={ onChange }>
+            <select class="form-select js-select" value={value} onChange={ onChange }>
                 { options }
             </select>
         </>
